7b204cc049a15b171c54c080a28c782ba787c89c
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-16 16:48:31 +0200

    new dpc()

--- R/dp.R
+++ R/dp.R
@@ -1,15 +1,10 @@
 #' A nicer deparse
 #'
-#' @description
 #' \code{dp} is a reimplementation of \code{\link[base]{dput}} and related
 #' functions. It tries its best to produce output that is easy to read
 #' (for humans), yet produces (almost) identical results to the input
 #' (for machines). This function is a generic, so other packages can easily
 #' provide implementations for the objects they define.
-#'
-#' Unlike \code{\link[base]{dput}} and \code{\link[base]{deparse}}, it creates
-#' a \code{call} object which can be evaluated.  Because the formatting may
-#' be important, it is retained as an attribute.
 #'
 #' @export
 dp <- function(x, ...) UseMethod("dp")
@@ -36,4 +31,15 @@ dp.POSIXlt <- function(x, ...) {
 
 dp_call <- function(call, argument) {
   paste0(call, "(", dp(argument), ")")
+}
+
+#' @rdname dp
+#'
+#' @description
+#' The \code{dpc} function leverages \code{dp} by creating
+#' a \code{call} object which can be evaluated but retains formatting
+#' (in the form of a \code{\link[base]{srcref}} attribute).
+dpc <- function(x, ...) {
+  text <- dp(x, ...)
+  as.srcref_call(srcfilecopy("<dpc>", text))
 }
--- R/srcref-call.R
+++ R/srcref-call.R
@@ -8,3 +8,36 @@
 srcref_call <- function(call, srcref) {
   stop("NYI")
 }
+
+#' @rdname srcref_call
+#' @export
+as.srcref_call <- function(x, ...) UseMethod("as.srcref_call")
+
+#' @export
+as.srcref_call.srcfile <- function(x, ...) {
+  ex <- parse_srcfile(x)
+  cl <- ex[[1L]]
+  class(cl) <- "srcref_call"
+  attr(cl, "srcref") <- attr(ex, "srcref")[[1]]
+  cl
+}
+
+#' @export
+print.srcref_call <- function(x, ..., useSource = TRUE) {
+  if (!useSource || is.null(srcref <- getSrcref(x)))
+    NextMethod()
+  else
+    print(srcref, ...)
+  invisible(x)
+}
+
+parse_srcfile <- function(x) {
+  parse(text = read_srcfile(x), keep.source = TRUE, srcfile = x)
+}
+
+read_srcfile <- function(x) {
+  # Can't pass conn to parse() for some reason, results are garbled
+  conn <- open(x, 1L)
+  on.exit(close(conn), add = TRUE)
+  readLines(conn)
+}
