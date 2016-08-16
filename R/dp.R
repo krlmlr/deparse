9716be4cdff19147198dec7264a7d825bc55dbbb
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-16 15:29:22 +0200

    stub

---
+++ R/dp.R
@@ -0,0 +1,17 @@
+#' A nicer deparse
+#'
+#' @description
+#' \code{dp} is a reimplementation of \code{\link[base]{dput}} and related
+#' functions. It tries its best to produce output that is easy to read
+#' (for humans), yet produces (almost) identical results to the input
+#' (for machines).
+#'
+#' Unlike \code{\link[base]{dput}} and \code{\link[base]{deparse}}, it creates
+#' a \code{language} object which can be evaluated.  Because the formatting may
+#' be important, it is retained as an attribute
+#'
+#' @export
+dp <- function(x, ...) UseMethod("dp")
+
+#' @export
+dp.default <- function(x) NULL
