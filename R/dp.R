9d40eea4ff3f825820171d492fa8e934f5a51c08
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-16 17:04:31 +0200

    dp.function()

--- R/dp.R
+++ R/dp.R
@@ -33,6 +33,32 @@ dp_call <- function(call, argument) {
   paste0(call, "(", dp(argument), ")")
 }
 
+#' @export
+dp.function <- function(x, ...) {
+  fun_in_namespace <- find_function_in_namespace(x)
+  if (is.null(fun_in_namespace))
+    NextMethod()
+  else {
+    paste0(dp(as.name(fun_in_namespace$ns)), "::", dp(as.name(fun_in_namespace$fun)))
+  }
+}
+
+find_function_in_namespace <- function(fun) {
+  env <- environment(fun)
+  if (!isNamespace(env))
+    return(NULL)
+
+  namespace_funs <- as.list(env)
+  namespace_funs <- namespace_funs[order(names(namespace_funs))]
+
+  same <- vapply(namespace_funs, identical, fun, FUN.VALUE = logical(1L))
+  same_name <- names(which(same))
+  if (length(same_name) == 0L)
+    return(NULL)
+
+  list(ns = getNamespaceName(env), fun = same_name[[1L]])
+}
+
 #' @rdname dp
 #'
 #' @description
