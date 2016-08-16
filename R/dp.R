792a9a368636e14e1f53a5ef9f33fd4b3c348b98
Kirill Müller <krlmlr+r@mailbox.org>
2016-08-16 15:42:40 +0200

    dp.default()

--- R/dp.R
+++ R/dp.R
@@ -7,11 +7,13 @@
 #' (for machines).
 #'
 #' Unlike \code{\link[base]{dput}} and \code{\link[base]{deparse}}, it creates
-#' a \code{language} object which can be evaluated.  Because the formatting may
-#' be important, it is retained as an attribute
+#' a \code{call} object which can be evaluated.  Because the formatting may
+#' be important, it is retained as an attribute.
 #'
 #' @export
 dp <- function(x, ...) UseMethod("dp")
 
 #' @export
-dp.default <- function(x) NULL
+dp.default <- function(x, ...) {
+  paste(deparse(x, 500L), collapse = "")
+}
